c914f6a1c10667e5f7f4b733b92196de58937efa
Chayan Kumar Saha <34272067+chayan7@users.noreply.github.com>
2020-11-20 00:30:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,8 @@ fi
 
 conv="$(conda -V 2>&1)"
 printf "$conv"
-
 condaExec="$(echo "$conv" | grep 'not' >&1)"
-printf "$condaExec"
+
 
 if [ "$condaExec" != "" ];then
 	printf "\\nPlease install Conda properly and run this again, Thanks. \\n"
@@ -22,6 +21,7 @@ if [ "$condaExec" = "" ];then
 fi
 
 
+
 THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
 THIS_FILE=$(basename "$0")
 THIS_PATH="$THIS_DIR/$THIS_FILE"
@@ -30,24 +30,22 @@ MAC_FILE="$ENV_PATH/eFlaGs.txt"
 LIN_FILE="$ENV_PATH/elinFlaGs.txt"
 
 if [ "$(uname)" = "Linux" ];then
-	conda create --name eFlaGs --file "$LIN_FILE"
-	printf "\\n"
+	conda create --name eFlaGs -f --file "$LIN_FILE"
 	printf "\\nPlease type the following command from any terminal to activate FlaGs environment and run:\\n"
-	printf "conda activate eFlaGs\\n"
+	printf ">> conda activate eFlaGs\\n"
 	printf "\\nTo deactivate please type:\\n"
-	printf "conda deactivate\\n"
+	printf ">> conda deactivate\\n"
 	printf "\\n"
-	exit 2
 fi
-
-
 if [ "$(uname)" = "Darwin" ]; then
-  conda create --name eFlaGs --file "$LIN_FILE"
-	printf "\\n"
+	conda create --name eFlaGs -f --file "$MAC_FILE"
 	printf "\\nPlease type the following command from any terminal to activate FlaGs environment and run:\\n"
-	printf "conda activate eFlaGs\\n"
+	printf ">> conda activate eFlaGs\\n"
 	printf "\\nTo deactivate please type:\\n"
-	printf "conda deactivate\\n"
+	printf ">> conda deactivate\\n"
 	printf "\\n"
-	exit 2
 fi
+
+printf "One can run FlaGs by activating eFlaGs environment from any terminal.\\n"
+printf "\\nInstallation complete, Thanks. \\n"
+exit 2
