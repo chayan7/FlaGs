e762b7f3a0041bab8a793b159947e6f1a4313e3d
Chayan Kumar Saha <34272067+chayan7@users.noreply.github.com>
2020-11-19 23:17:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,82 +1,74 @@
 #!/bin/sh
+
 unset DYLD_LIBRARY_PATH
 if ! echo "$0" | grep '\.sh$' > /dev/null; then
     printf 'Please run using "bash" or "sh", but not "." or "source"\\n' >&2
     return 1
 fi
 
+conv="$(conda -V 2>&1)"
+if [ "$ans" = "conda: command not found" ];then
+	printf " Please install Conda properly and run this again, Thanks. "
+fi
+if [ "$ans" != "conda: command not found" ];then
+	printf "Adding FlaGs environment with all dependencies"
+fi
+
+
 THIS_DIR=$(DIRNAME=$(dirname "$0"); cd "$DIRNAME"; pwd)
 THIS_FILE=$(basename "$0")
 THIS_PATH="$THIS_DIR/$THIS_FILE"
-ENV_PATH="$THIS_DIR/bin"
-cd "$ENV_PATH"
-wget http://130.239.193.227/html/eFlaGs.tar.gz
-cd "../"
-ENV_FILE="$ENV_PATH/eFlaGs.tar.gz"
-rm -rf $ENV_PATH/env
-mkdir $ENV_PATH/env
-tarPath="$ENV_PATH/env/"
-PATH_TO_ADD="$ENV_PATH/env/bin/"
-PATH_TO_Act="$ENV_PATH/env/bin"
-tar -xvzf $ENV_FILE -C $tarPath
+ENV_PATH="$THIS_DIR/env"
+MAC_FILE="$ENV_PATH/eFlaGs.txt"
+LIN_FILE="$ENV_PATH/elinFlaGs.txt"
 
 if [ "$(uname)" = "Linux" ];then
-	if [ -f "$HOME"/.bashrc ]; then
-		BASH_RC="$HOME"/.bashrc
-		edit=".bashrc"
-	fi
-	if ! [ -f "$HOME"/.bashrc ]; then
-		BASH_RC="$HOME"/.bashrc
-		edit=".bashrc"
-	fi
+	conda create --name eFlaGs --file "$LIN_FILE"
 	printf "\\n"
-	printf "Do you wish the installer to prepend the FlaGs install location\\n"
-	printf "to PATH in your %s ? [yes|no]\\n" "$BASH_RC"
+	printf "Do you wish the installer to activate FlaGs environment ?\\n"
 	printf "[yes] >>> "
 	read -r ans
 	if [ "$ans" != "yes" ] && [ "$ans" != "Yes" ] && [ "$ans" != "YES" ] && [ "$ans" != "y" ]   && [ "$ans" != "Y" ]; then
-		printf "\\nPlease add the following line to your $edit and restart terminal to run FlaGs:\\n"
-		printf "export PATH="$PATH_TO_ADD":\$PATH\\n"
+		printf "\\nPlease type the following command from a terminal to activate FlaGs environment and run:\\n"
+		printf "conda activate eFlaGs"
+		printf "To deactivate please type:"
+		printf "conda deactivate"
 		printf "\\n"
 		exit 2
 	fi
 	if ! [ "$ans" != "yes" ] && [ "$ans" != "Yes" ] && [ "$ans" != "YES" ] && [ "$ans" != "y" ]   && [ "$ans" != "Y" ]; then
-		printf "Appending source %s/activate to %s\\n" "$PATH_TO_Act" "$BASH_RC"
-		echo "export PATH=\""$PATH_TO_ADD":\$PATH"\" >> "$BASH_RC"
+		conda activate eFlaGs
+		printf "\\nActivated FlaGs environment"
+		printf "\\nFor this change to become active, you have to open a new terminal.\\n"
 		printf "\\n"
-		printf "For this change to become active, you have to open a new terminal.\\n"
+		printf "To deactivate please type:"
+		printf "conda deactivate"
 		printf "\\n"
 	fi
 fi
 
 
 if [ "$(uname)" = "Darwin" ]; then
-	if [ -f "$HOME"/.bash_profile ]; then
-		BASH_RC="$HOME"/.bash_profile
-		edit=".bash_profile"
-		echo "$BASH_RC"
-	fi
-	if ! [ -f "$HOME"/.bash_profile ]; then
-		touch "$HOME"/.bash_profile
-		BASH_RC="$HOME"/.bash_profile
-		edit=".bash_profile"
-	fi
+	conda create --name eFlaGs --file "$MAC_FILE"
 	printf "\\n"
-	printf "Do you wish the installer to prepend the FlaGs install location\\n"
-	printf "to PATH in your %s ? [yes|no]\\n" "$BASH_RC"
+	printf "Do you wish the installer to activate FlaGs environment ?\\n"
 	printf "[yes] >>> "
 	read -r ans
 	if [ "$ans" != "yes" ] && [ "$ans" != "Yes" ] && [ "$ans" != "YES" ] && [ "$ans" != "y" ]   && [ "$ans" != "Y" ]; then
-		printf "\\nPlease add the following line to your $edit and restart terminal to run FlaGs:\\n"
-		printf "export PATH="$PATH_TO_ADD":\$PATH\\n"
+		printf "\\nPlease type the following command from a terminal to activate FlaGs environment and run:\\n"
+		printf "conda activate eFlaGs"
+		printf "To deactivate please type:"
+		printf "conda deactivate"
 		printf "\\n"
 		exit 2
 	fi
 	if ! [ "$ans" != "yes" ] && [ "$ans" != "Yes" ] && [ "$ans" != "YES" ] && [ "$ans" != "y" ]   && [ "$ans" != "Y" ]; then
-		printf "Appending source %s/activate to %s\\n" "$PATH_TO_Act" "$BASH_RC"
-		echo "export PATH=\""$PATH_TO_ADD":\$PATH"\" >> "$BASH_RC"
+		conda activate eFlaGs
+		printf "\\nActivated FlaGs environment"
+		printf "\\nFor this change to become active, you have to open a new terminal.\\n"
 		printf "\\n"
-		printf "For this change to become active, you have to open a new terminal.\\n"
+		printf "To deactivate please type:"
+		printf "conda deactivate"
 		printf "\\n"
 	fi
 fi
